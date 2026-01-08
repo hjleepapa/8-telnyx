5f4fb6a92ff05e4dcc01ec1a06ab1bd24409c8ba
HJ <hjleegcti@gmail.com>
2026-01-07 16:50:54 -0800

    Add mortgage models to models __init__.py for startup import
    - Import mortgage models at startup for Flask-Migrate autogenerate support
    - Makes MortgageApplication, MortgageDocument, MortgageDebt, MortgageApplicationNote available
    - Also exports ApplicationStatus, DocumentType, DocumentStatus enums
    - Ensures models are registered with SQLAlchemy Base.metadata at startup
    - Models will now be automatically imported when convonet.models is imported

--- convonet/models/__init__.py
+++ convonet/models/__init__.py
@@ -2,6 +2,15 @@
 
 from .base import Base  # re-export for convenience
 from .user_models import User, Team, TeamMembership, TeamRole, UserRole
+from .mortgage_models import (
+    MortgageApplication,
+    MortgageDocument,
+    MortgageDebt,
+    MortgageApplicationNote,
+    ApplicationStatus,
+    DocumentType,
+    DocumentStatus,
+)
 
 __all__ = [
     "Base",
@@ -10,4 +19,12 @@ __all__ = [
     "TeamMembership",
     "TeamRole",
     "UserRole",
+    # Mortgage models
+    "MortgageApplication",
+    "MortgageDocument",
+    "MortgageDebt",
+    "MortgageApplicationNote",
+    "ApplicationStatus",
+    "DocumentType",
+    "DocumentStatus",
 ]
